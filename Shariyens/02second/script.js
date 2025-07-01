f56b788f6cf03648dcd7fd9d32a4dc4947072aac
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-01 14:39:47 +0530

    1/7

---
+++ Shariyens/02second/script.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import express from 'express'
+const app = express()
+const {dirname: __dirname} = import.meta;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const dirname = app.use(express.static(path.join(__dirname, 'public')))
+// console.log(__dirname);
+
+app.set('view engine', 'ejs')
+
+
+app.get("/", function(req, res){
+    res.render('index')
+})
+app.get("/api/v1/:username", function(req, res){
+    res.send(`welcome ${req.params.username}`)
+})
+app.get("/api/v1/:username/:age", function(req, res){
+    res.send(`welcome ${req.params.username} your age is ${req.params.age}`)
+    
+})
+
+
+
+app.listen(3000)
