239269fb8ee118e5c57404e0094fb2a0c6ba5e69
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-22 23:30:20 +0530

    how to use bcrypt and encode your password

--- Shariyens/05Authentication/app.js
+++ Shariyens/05Authentication/app.js
@@ -1,10 +1,25 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
+// import cookieParser from 'cookie-parser'
+// app.use(cookieParser())
 
+// app.get('/', function(req, res){
+//     res.cookie("name", "Rajan")
+//     res.send('done')
+// })
 
-app.get('/', function(req, res){
-    res.cookie("name", "Rajan")
-    res.send('done')
-})
+import bcrypt from "bcrypt";
+app.get("/", function (req, res) {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("helloo", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
+});
 
-app.listen(3000)
+app.get("/read", function (req, res) {
+  console.log(req.cookies);
+  res.send("Read page");
+});
+
+app.listen(3000);
