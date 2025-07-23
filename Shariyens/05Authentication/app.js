57ba2bec98efb5ea1f222b079fde605121ab97d2
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-23 23:51:55 +0530

    jwt token

--- Shariyens/05Authentication/app.js
+++ Shariyens/05Authentication/app.js
@@ -1,21 +1,10 @@
 import express from "express";
 const app = express();
-// import cookieParser from 'cookie-parser'
-// app.use(cookieParser())
 
-// app.get('/', function(req, res){
-//     res.cookie("name", "Rajan")
-//     res.send('done')
-// })
 
-import bcrypt from "bcrypt";
-app.get("/", function (req, res) {
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash("helloo", salt, function (err, hash) {
-      console.log(hash);
-    });
-  });
-});
+app.get('/', function(req, res){
+  
+})
 
 app.get("/read", function (req, res) {
   console.log(req.cookies);
