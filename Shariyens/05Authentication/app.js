c7d4e16685e0727695a67f2d81c14bf71a65027d
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-24 15:00:00 +0530

    Added authentication endpoints

--- Shariyens/05Authentication/app.js
+++ Shariyens/05Authentication/app.js
@@ -2,10 +2,8 @@ import express from "express";
 const app = express();
 
 
-app.get('/', function(req, res){
-  
+app.get('/', function(req, res){ 
 })
-
 app.get("/read", function (req, res) {
   console.log(req.cookies);
   res.send("Read page");
